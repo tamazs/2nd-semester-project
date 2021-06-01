638cf3b39b778d177b17b975c262d01204b51b45
tamazs <majortamas313@gmail.com>
2021-06-01 21:42:56 +0200

    about responsive tbd, translations groups added

--- src/components/AboutMain.vue
+++ src/components/AboutMain.vue
@@ -26,7 +26,7 @@
                         <v-img
                         class="white--text align-end"
                         height="400px"
-                        src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+                        :src="require('/src/assets/Fun.jpg')"
                         >
                         <v-card-title id="smalltitle">{{ $t('about.smalltitle1') }}</v-card-title>
                         </v-img>
@@ -39,7 +39,7 @@
                         <v-img
                         class="white--text align-end"
                         height="400px"
-                        src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+                        :src="require('/src/assets/adventures.jpg')"
                         >
                         <v-card-title id="smalltitle">{{ $t('about.smalltitle2') }}</v-card-title>
                         </v-img>
@@ -54,7 +54,7 @@
                         <v-img
                         class="white--text align-end"
                         height="400px"
-                        src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+                        :src="require('/src/assets/Community.jpg')"
                         >
                         <v-card-title id="smalltitle">{{ $t('about.smalltitle3') }}</v-card-title>
                         </v-img>
@@ -67,7 +67,7 @@
                         <v-img
                         class="white--text align-end"
                         height="400px"
-                        src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+                        :src="require('/src/assets/Friendship.jpg')"
                         >
                         <v-card-title id="smalltitle">{{ $t('about.smalltitle4') }}</v-card-title>
                         </v-img>
@@ -88,10 +88,173 @@
 @import url("https://use.typekit.net/srn1jpl.css");
 @import url('https://fonts.googleapis.com/css2?family=Alfa+Slab+One&display=swap');
 
-@media only screen and (min-width: 688px) {
+#aboutmain {
+        display: grid;
+        grid-template-columns: 1fr;
+        grid-template-rows: 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+        grid-template-areas: 
+        "boxau1"
+        "boxau2"
+        "boxau3"
+        "boxau4"
+        "boxau5"
+        "boxau6"
+        "boxau7";
+    }
+
+    #boxau1 {
+        grid-area: boxau1;
+        height: 500px;
+        background-color: white;
+        padding: 30px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    #boxau2 {
+        grid-area: boxau2;
+        height: 500px;
+        padding: 30px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    #boxau3 {
+        grid-area: boxau3;
+        height: 500px;
+        background-color: #0E7432;
+        padding: 30px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    #boxau4 {
+        grid-area: boxau4;
+        height: 500px;
+        display: block;
+        margin: 0;
+        padding: 0;
+    }
+
+    #boxau5 {
+        grid-area: boxau5;
+        height: auto;
+        background-color: #0E7432;
+        padding: 30px;
+    }
+
+    #boxau6 {
+        grid-area: boxau6;
+        height: 500px;
+        background-color: white;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 30px;
+    }
+
+    #boxau7 {
+        grid-area: boxau7;
+        height: 500px;
+        background-color: white;
+        padding: 30px;
+    }
+
+    #abouttitle {
+        font-family: 'Alfa Slab One', cursive;
+        font-size: 2em;
+        color: #0E7432;
+        letter-spacing: 3px;
+        margin-bottom: 20px;
+        text-align: center;
+        left: 50%;
+        right: 50%;
+    }
+
+    #abouttext {
+        color: #0E7432;
+        max-width: 400px;
+        text-align: center;
+    }
+
+    #video {
+        height: 500px;
+        width: 100%;
+        display: block;
+        margin: 0;
+        padding: 0;
+    }
+
+    #goalstitle {
+        font-family: 'Alfa Slab One', cursive;
+        font-size: 2em;
+        color: white;
+        letter-spacing: 3px;
+        margin-bottom: 50px;
+        text-align: center;
+    }
+
+    #goaltext {
+        color: white;
+        max-width: 800px;
+        margin-bottom: 50px;
+        text-align: center;
+    }
+
+    #valuetitle {
+        font-family: 'Alfa Slab One', cursive;
+        font-size: 2em;
+        color: white;
+        letter-spacing: 3px;
+        margin-bottom: 60px;
+        text-align: center;
+    }
+
+    #dresstitle {
+        font-family: 'Alfa Slab One', cursive;
+        font-size: 2em;
+        color: #0E7432;
+        letter-spacing: 3px;
+        margin-bottom: 20px;
+        text-align: center;
+    }
+
+    #smalltitle {
+        font-family: 'Alfa Slab One', cursive;
+    }
+
+    #powellpic {
+        max-height: 600px;
+        max-width: 500px;
+        padding: 0;
+    }
+
+    #dress {
+        max-height: 500px;
+        max-width: 400px;
+    }
+
+    #aboutrow1 {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+    }
+
+    #aboutrow2 {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+    }
+
+@media only screen and (min-width: 890px) {
 
     #aboutmain {
-        margin-top: 200px;
+        height: auto;
         display: grid;
         grid-template-columns: 1fr 1fr;
         grid-template-rows: 1fr 1fr 1fr 1fr 1fr;
@@ -109,6 +272,10 @@
         background-color: white;
         padding: 30px;
         width: 1fr;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
     }
 
     #boxau2 {
@@ -116,22 +283,30 @@
         height: auto;
         padding: 30px;
         width: 1fr;
+        display: flex;
+        justify-content: center;
+        align-items: center;
     }
 
     #boxau3 {
         grid-area: boxau3;
-        height: auto;
+        height: 400px;
         background-color: #0E7432;
         padding: 30px;
         width: 2fr;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
     }
 
     #boxau4 {
         grid-area: boxau4;
         height: auto;
+        width: 2fr;
+        display: block;
         margin: 0;
         padding: 0;
-        width: 2fr;
     }
 
     #boxau5 {
@@ -180,7 +355,10 @@
 
     #video {
         height: auto;
-        width: 100vw;
+        width: 100%;
+        display: block;
+        margin: 0;
+        padding: 0;
     }
 
     #goalstitle {
@@ -188,7 +366,14 @@
         font-size: 3em;
         color: white;
         letter-spacing: 3px;
-        margin-bottom: 20px;
+        margin-bottom: 50px;
+        text-align: center;
+    }
+
+    #goaltext {
+        color: white;
+        max-width: 800px;
+        margin-bottom: 50px;
         text-align: center;
     }
 
@@ -215,8 +400,8 @@
     }
 
     #powellpic {
-        height: 500px;
-        width: 1fr;
+        height: 800px;
+        width: 200px;
     }
 
     #dress {
@@ -224,7 +409,7 @@
         max-width: 600px;
         margin: 0;
         padding: 0;
-        top: 15%;
+        top: 25%;
     }
 
     #aboutrow1 {
---
+++ src/components/Group1.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="group1body">
+        <div id="group1text"></div>
+        <div id="group1pic"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Group1"
+}
+</script>
+
+<style lang="scss">
+
+</style>

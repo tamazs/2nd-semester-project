4bcac5cfbd434eed1ce1ab75f80a42187dfe2809
tamazs <majortamas313@gmail.com>
2021-06-02 16:44:21 +0200

    map desc added

--- src/components/Location.vue
+++ src/components/Location.vue
@@ -5,6 +5,7 @@
         </h2>
         <div id="mapembed">
             <iframe id='map' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2261.0403699158423!2d8.446289515919961!3d55.47939418048115!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x464b20d8442ed02d%3A0x2a118d81eb2fdc8c!2sBakkevej%2014%2C%206700%20Esbjerg!5e0!3m2!1shu!2sdk!4v1621952737638!5m2!1shu!2sdk" width="900" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+            <p id="locationtext">{{ $t('location.text') }}</p>
         </div>
     </div>
 </template>
@@ -35,14 +36,23 @@ export default {
     }
 
     #map {
-        margin-bottom: 90px;
-        width: 600px;
+        margin-bottom: 30px;
+        width: 100%;
         height: 400px;
+    }
+
+    #locationtext{
+        color: #133454;
+        max-width: 100%;
+        text-align: center;
     }
 
     #mapembed {
         display: flex;
         justify-content: center;
+        flex-direction: column;
+        align-items: center;
+        margin-bottom: 90px;
     }
 
 @media only screen and (min-width: 688px) {
@@ -63,14 +73,23 @@ export default {
     }
 
     #map {
-        margin-bottom: 90px;
+        margin-bottom: 30px;
         width: 900px;
         height: 600px;
+    }
+
+    #locationtext{
+        color: #133454;
+        max-width: 900px;
+        text-align: center;
     }
 
     #mapembed {
         display: flex;
         justify-content: center;
+        flex-direction: column;
+        align-items: center;
+        margin-bottom: 90px;
     }
 }
 
